0002d89ccce7af4a02c63686b916f7660b82965d
unknown <fabianamendez742@gmail.com>
2023-06-02 22:06:51 -0300

    terminado

--- JS/form.js
+++ JS/form.js
@@ -1,4 +1,5 @@
-let cantidadEntradas;
+
+/*let cantidadEntradas;
 let descuento;
 let resultado;
 let totalPago = document.getElementById("totalPago");
@@ -13,7 +14,7 @@ function obtenerCantidadEntradas(){
 }
 
 function obtenerDescuento(){
-    descuento = parseFloat(document.getElementById("categoria").value);
+    descuento = parseFloat(document.getElementById("tipo").value);
     return descuento;
 }
 
@@ -24,11 +25,11 @@ function precioFinal(cantidadEntradas, descuento){
 
 function mostrarPrecioFinal(){
     precioFinal(cantidadEntradas, descuento);
-    return total.innerHTML = `${ precioFinal(cantidadEntradas, descuento)}`
+    return totalPago.innerHTML = `${precioFinal(cantidadEntradas, descuento)}`
 }
 
 function borrarDatos(cantidadEntradas, descuento){
-    cantidadEntradas = ""
+    cantidadEntradas = "";
     descuento = ""
     totalPago.innerHTML = ""
     return console.log(cantidadEntradas, descuento);
@@ -36,8 +37,31 @@ function borrarDatos(cantidadEntradas, descuento){
 
 
 cantidadEntradas = document.getElementById("cantidadEntradas").addEventListener("change", obtenerCantidad);
-descuento = document.getElementById("categoria").addEventListener("change", obtenerDescuento);
+descuento = document.getElementById("tipo").addEventListener("change", obtenerDescuento);
 precioFinal(cantidadEntradas, descuento);
-resumen.onclick = mostrarPrecioFinal
-borrar.onclick = borrarDatos
+resumen.onclick = mostrarPrecioFinal;
+borrar.onclick = borrarDatos*/
 
+function calcularTotal() {
+            let valorUnitario = 200;
+            let cantidad = parseInt(document.getElementById("cantidad").value);
+            let categoria = document.getElementById("categoria").value;
+            let descuento = 0;
+            
+            if (categoria === "estudiante") {
+                descuento = 0.80;
+            } else if (categoria === "traineer") {
+                descuento = 0.50;
+            } else if (categoria === "junior") {
+                descuento = 0.15;
+            }
+            
+            var total = valorUnitario * cantidad * (1 - descuento);
+            
+            document.getElementById("total").value = total;
+        }
+        
+        function borrarTotal() {
+            document.getElementById("total").value = "";
+        }
+    
